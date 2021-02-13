8b76853525b2ff741621665a01f42bcb13a48934
jgruenbaum <jeff.gruenbaum@gmail.com>
2021-02-12 21:58:43 -0600

    finished html example

--- src/htmlfactory.py
+++ src/htmlfactory.py
@@ -1,11 +1,13 @@
 class TagFactory():
     """A tag object that has a tag, inner_html, and attribute dictionary."""
-    def __init__(self, tag_and_class_str: str, inner_html, **kwargs):
-        self.list_of_tags = ("body", "document", "div", "h1")
+    def __init__(self, tag_and_class_str: str, inner_html,
+                 children=(), **kwargs):
+        self.list_of_tags = ("body", "document", "div", "h1", "form",
+                             "input", "small", "button", "label")
         self.inner_html = inner_html
         self.attr_dict = kwargs
         self.cleanse_tag_and_class_str(tag_and_class_str)
-        self.children = ()
+        self.children = children
 
     def split_str_by_period(self, str):
         return str.split(".")
@@ -63,13 +65,47 @@ def test_ouput():
 
 
 def example_html_creation():
-    body_tag_object = TagFactory("body", '')
-    body_tag_object.children = (
+    body_tag_object = TagFactory("body", '', (
         TagFactory("div.col-10.col-lg-9.d-inline-block",
                    'inside the tags', id="target-div"),
-        TagFactory("div.col-10.col-lg-3.d-inline-block", 'well how about that')
-    )
+        TagFactory("div.col-10.col-lg-3.d-inline-block",
+                   'well how about that'),
+        TagFactory("form.input-handler", '', (
+            TagFactory("div.form-group", '', (
+                TagFactory("label", 'Email Address', foor="exampleInputEmail1"),
+                TagFactory("input.form-control", '', id="exampleInputEmail1", ariadescribedby="emailHelp", placeholder="Enter email"),
+                TagFactory("small.form-text.text-muted", "We'll never share your email with anyone else.", id="emailHelp"),
+            )),
+            TagFactory("div.form-group", '', (
+                TagFactory("label", 'Password', foor="exampleInputPassword1"),
+                TagFactory("input.form-control", '', id="exampleInputPassword1", placeholder="Password"),
+            )),
+            TagFactory("div.form-check", '', (
+                TagFactory("input.form-check-input", '', id="exampleCheck1"),
+                TagFactory("label.form-check-label", 'Check me out', foor="exampleCheck1"),
+            )),
+            TagFactory("button.btn.btn-primary", 'Submit', type="submit")
+        ))
+    ))
     print(body_tag_object)
+    """Expected output -->
+    <body>
+        <div class='col-10 col-lg-9 d-inline-block' id='target-div'>inside the tags</div>
+        <div class='col-10 col-lg-3 d-inline-block'>well how about that</div>
+        <form class='input-handler'>
+            <div class='form-group'><label class=' foor='exampleInputEmail1'>Email Address</label>
+                <input class='form-control' id='exampleInputEmail1' ariadescribedby='emailHelp' placeholder='Enter email'></input>
+                <small class='form-text text-muted' id='emailHelp'>We'll never share your email with anyone else.</small>
+            </div>
+            <div class='form-group'><label class=' foor='exampleInputPassword1'>Password</label>
+                <input class='form-control' id='exampleInputPassword1' placeholder='Password'></input>
+            </div>
+            <div class='form-check'><input class='form-check-input' id='exampleCheck1'></input>
+                <label class='form-check-label' foor='exampleCheck1'>Check me out</label>
+            </div>
+            <button class='btn btn-primary' type='submit'>Submit</button>
+        </form>
+    </body>"""
 
 
 example_html_creation()
