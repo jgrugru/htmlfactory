f02ff736179751ca75c104c691a366ac0b33dc89
jgruenbaum <jeff.gruenbaum@gmail.com>
2021-02-12 18:58:26 -0600

    created TagObject class

--- src/htmlfactory.py
+++ src/htmlfactory.py
@@ -12,7 +12,6 @@
 # print(fill_html_tag("test"))
 
 
-list_of_tags = ("body", "document", "div", "h1")
 
 
 def tag_function_factory(tag_list):
@@ -23,31 +22,56 @@ def tag_function_factory(tag_list):
 
 
 def make_html_tag(tag):
-    def make_tags(content, *args):
-        # print("inside make tags", args)
-        return "<" + tag + attr_concatenater(*args) + ">" + content + "</" + tag + ">"
+    def make_tags(content, *args, **kwargs):
+        return "<" + tag + attr_concatenater(*args, **kwargs) + ">" + content + "</" + tag + ">"
     
     return make_tags
 
-def attr_concatenater(*args):
+def attr_concatenater(*args, **kwargs):
     """This function creates a string with all of the html attributes concatenated together."""
-    if(not bool(len(args))):
+    if(len(args)==0 and len(kwargs)==0):
         return ''
-    class_str = " class="
-    for counter, class_element in enumerate(args):
-        if(counter != 0):
-            class_str += " " + class_element
+    attr_str = ""
+    for attr in kwargs:
+        if attr == 'klass':
+            attr_str += ' ' + 'class' + '=' + "\'" + kwargs[attr] + "\'"
         else:
-            class_str += class_element
-    return class_str
+            attr_str += ' ' + attr + '=' + "\'" + kwargs[attr] + "\'"
+    return attr_str
 
 
-html_factory = tag_function_factory(list_of_tags)
-# html_factory['div']("happy birthday", klass="container w-100", id="target-div")
+# html_factory = tag_function_factory(list_of_tags)
+# print(html_factory['div']("happy birthday", klass="container w-100", id="target-div"))
 
 
-# print(html_factory['div'](''))
-# html_factory("div", klass="container w-100", id="target-div")
+#html_factory("div.col-10.col-lg-9.d-inline-block") --> self.tag = div
+#                                                       self.klass = ['col-10','col-lg-9',...]
+#                                                       self.attr['id'] = target-div
+#
 
-# create an outer div with bootstrap class elements
-# 
+
+class TagFactory():
+
+    def __init__(self, tag_and_class_str: str, inner_html, **kwargs):
+        self.list_of_tags = ("body", "document", "div", "h1")
+        self.inner_html = inner_html
+        self.attr_dict = kwargs
+        self.cleanse_tag_and_class_str(tag_and_class_str)
+        print(kwargs)
+
+    def split_str_by_period(self, str):
+        return str.split(".")
+
+    def cleanse_tag_and_class_str(self, tag_and_class_str):
+        tag_and_class_list = self.split_str_by_period(tag_and_class_str)
+        if tag_and_class_list[0] in self.list_of_tags:
+            self.tag = tag_and_class_list[0]
+        else:
+            self.tag = None
+        self.class_list = tag_and_class_list[1:]
+
+
+TagFactory("div.col-10.col-lg-9.d-inline-block", 'inside the tags', id="target-div")
+
+def test_ouput():
+    assert(str(TagFactory("div.col-10.col-lg-9.d-inline-block", '', id="target-div")) == "<div class='col-10 col-lg-9 d-inline-block' id='target-div'>inside the tags</div>")
